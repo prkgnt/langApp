ab59597f94f82c0e15b023a91aaa2ab4cc57c82f
prkgnt <pgt258258@gmail.com>
2023-02-27 20:47:54 +0900

    4.8) loop, sequence 구현

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Animated, Easing } from "react-native";
+import { Animated, Dimensions, Easing } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -18,20 +18,22 @@ const Box = styled.View`
 
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
+
 export default function App() {
-  const [up, setUp] = useState(false);
   //Y = new Animated.Value(0); 로 두게 되면 스테이트가 변경될 때 마다 재렌더링 되서 Y값이 0으로 초기화됨
   //useRef로 감싸고 안에 초기값을 주면 재렌더링 되도 현재 값이 유지됨
-  const POSITION = useRef(new Animated.ValueXY({ x: 0, y: 300 })).current;
+  const POSITION = useRef(
+    new Animated.ValueXY({
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    })
+  ).current;
   const toggleUp = () => {
     setUp((prev) => !prev);
   };
-  const opacityValue = POSITION.y.interpolate({
-    //inputRange의 값들을 outputRange의 값으로 자동 치환
-    inputRange: [-300, -100, 100, 300],
-    outputRange: [1, 0.1, 0.1, 1],
-  });
   const borderRadius = POSITION.y.interpolate({
+    //inputRange의 값들을 outputRange의 값으로 자동 치환
     inputRange: [-300, 300],
     outputRange: [100, 0],
   });
@@ -43,13 +45,27 @@ export default function App() {
     inputRange: [-300, 300],
     outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
   });
+
+  const topLeft = Animated.timing(POSITION, {
+    toValue: { x: -SCREEN_WIDTH / 2 + 100, y: -SCREEN_HEIGHT / 2 + 100 },
+    useNativeDriver: false,
+  });
+  const bottomLeft = Animated.timing(POSITION, {
+    toValue: { x: -SCREEN_WIDTH / 2 + 100, y: SCREEN_HEIGHT / 2 - 100 },
+    useNativeDriver: false,
+  });
+  const topRight = Animated.timing(POSITION, {
+    toValue: { x: SCREEN_WIDTH / 2 - 100, y: -SCREEN_HEIGHT / 2 + 100 },
+    useNativeDriver: false,
+  });
+  const bottomRight = Animated.timing(POSITION, {
+    toValue: { x: SCREEN_WIDTH / 2 - 100, y: SCREEN_HEIGHT / 2 - 100 },
+    useNativeDriver: false,
+  });
   const moveUp = () => {
-    Animated.timing(POSITION, {
-      toValue: up ? { x: 0, y: 300 } : { x: 0, y: -300 },
-      useNativeDriver: false,
-      duration: 1000,
-    }).start(toggleUp);
-    //start(함수) 하면 애니메이션이 끝나고 함수가 실행됨
+    Animated.loop(
+      Animated.sequence([bottomLeft, bottomRight, topRight, topLeft])
+    ).start();
   };
   return (
     <Container>
@@ -58,8 +74,11 @@ export default function App() {
           style={{
             borderRadius,
             backgroundColor: bgColor,
-            transform: [{ translateY: POSITION.y }, { rotate: rotation }],
-            opacity: opacityValue,
+            transform: [
+              ...POSITION.getTranslateTransform(),
+              // == [{ translateX: POSITION.x },{ translateY: POSITION.y }]
+              //{ rotate: rotation },
+            ],
           }}
         />
       </Wrapper>
