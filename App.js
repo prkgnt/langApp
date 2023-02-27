e10568087bd675ec1cf3ecd142478e64f9f94b02
prkgnt <pgt258258@gmail.com>
2023-02-27 21:39:15 +0900

    4.10) PanResponder를 이용한 터치 제스처 구현

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Animated, Dimensions, Easing } from "react-native";
+import { Animated, PanResponder } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -18,70 +18,60 @@ const Box = styled.View`
 
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
-
 export default function App() {
   //Y = new Animated.Value(0); 로 두게 되면 스테이트가 변경될 때 마다 재렌더링 되서 Y값이 0으로 초기화됨
   //useRef로 감싸고 안에 초기값을 주면 재렌더링 되도 현재 값이 유지됨
   const POSITION = useRef(
     new Animated.ValueXY({
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
+      x: 0,
+      y: 0,
     })
   ).current;
-  const toggleUp = () => {
-    setUp((prev) => !prev);
-  };
+
+  const panResponder = useRef(
+    PanResponder.create({
+      //터치를 했을 때 PanResponder를 Active 해야 될까요?
+      // Should we become active when user press down circle(View)?
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx, dy }) => {
+        POSITION.setValue({
+          x: dx,
+          y: dy,
+        });
+      },
+      onPanResponderRelease: () => {
+        Animated.spring(POSITION, {
+          toValue: { x: 0, y: 0 },
+          useNativeDriver: false,
+        }).start();
+      },
+    })
+  ).current;
+
   const borderRadius = POSITION.y.interpolate({
     //inputRange의 값들을 outputRange의 값으로 자동 치환
     inputRange: [-300, 300],
     outputRange: [100, 0],
-  });
-  const rotation = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: ["-360deg", "360deg"],
   });
   const bgColor = POSITION.y.interpolate({
     inputRange: [-300, 300],
     outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
   });
 
-  const topLeft = Animated.timing(POSITION, {
-    toValue: { x: -SCREEN_WIDTH / 2 + 100, y: -SCREEN_HEIGHT / 2 + 100 },
-    useNativeDriver: false,
-  });
-  const bottomLeft = Animated.timing(POSITION, {
-    toValue: { x: -SCREEN_WIDTH / 2 + 100, y: SCREEN_HEIGHT / 2 - 100 },
-    useNativeDriver: false,
-  });
-  const topRight = Animated.timing(POSITION, {
-    toValue: { x: SCREEN_WIDTH / 2 - 100, y: -SCREEN_HEIGHT / 2 + 100 },
-    useNativeDriver: false,
-  });
-  const bottomRight = Animated.timing(POSITION, {
-    toValue: { x: SCREEN_WIDTH / 2 - 100, y: SCREEN_HEIGHT / 2 - 100 },
-    useNativeDriver: false,
-  });
-  const moveUp = () => {
-    Animated.loop(
-      Animated.sequence([bottomLeft, bottomRight, topRight, topLeft])
-    ).start();
-  };
   return (
     <Container>
-      <Wrapper onPress={moveUp}>
-        <AnimatedBox
-          style={{
-            borderRadius,
-            backgroundColor: bgColor,
-            transform: [
-              ...POSITION.getTranslateTransform(),
-              // == [{ translateX: POSITION.x },{ translateY: POSITION.y }]
-              //{ rotate: rotation },
-            ],
-          }}
-        />
-      </Wrapper>
+      <AnimatedBox
+        {...panResponder.panHandlers}
+        style={{
+          borderRadius,
+          backgroundColor: bgColor,
+          transform: [
+            ...POSITION.getTranslateTransform(),
+            // == [{ translateX: POSITION.x },{ translateY: POSITION.y }]
+            //{ rotate: rotation },
+          ],
+        }}
+      />
     </Container>
   );
   //styled 자동완성
